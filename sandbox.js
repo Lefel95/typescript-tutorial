f24483ec547caeda9b19451802db233ec0071df5
felipebgomes <felipe.gomes@turbi.com.br>
2020-07-25 02:53:40 -0300

    understand basic types, inference and typed parameter after that compiled ts to js

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,14 @@
+//Here we understand inference the TS understand the type of the variable in assignment
 var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 20;
+var isBlackBelt = false;
+//character = 20 it becomes a error;
+character = 'luigi';
+// age = 'yoshi'; is wrong again!
+age = 40;
+// isBlackBelt = 'yes' wrong
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI; // all js libraries are used on TS
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,20 @@
-const character = 'mario';
 
-console.log(character);
+//Here we understand inference the TS understand the type of the variable in assignment
+let character = 'mario';
+let age = 20;
+let isBlackBelt = false;
 
-const inputs = document.querySelectorAll('input');
+//character = 20 it becomes a error;
+character = 'luigi';
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// age = 'yoshi'; is wrong again!
+age = 40;
+
+// isBlackBelt = 'yes' wrong
+isBlackBelt = true;
+
+const circ = (diameter: number) => { // the parameter : type notation is used here, we can ommit the type of the parameter if we want
+    return diameter * Math.PI; // all js libraries are used on TS
+};
+
+console.log(circ(7.5));
